55b1c64c5741243b0813e7a439d1cf758dc909ad
Michael Cimino <saibotshamtul@gmail.com>
2020-08-25 15:05:56 +0000

    fixed the spacing ... using tabs instead of 4 spaces

--- mydemoapp.py
+++ mydemoapp.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    #return "Hello! This is a basic Flask app."
+	#return "Hello! This is a basic Flask app."
 	return Response("Hello! This is a <b>basic</b> Flask app.",mimetype="text/html")
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+	app.run("0.0.0.0")
