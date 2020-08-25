0a18b9c112e04de39079e370c607ffe91c39a0da
Michael Cimino <saibotshamtul@gmail.com>
2020-08-25 14:59:48 +0000

    added a basic Response with mimetype

--- mydemoapp.py
+++ mydemoapp.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello! This is a basic Flask app. "
+    #return "Hello! This is a basic Flask app."
+	return Response("Hello! This is a <b>basic</b> Flask app.",mimetype="text/html")
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
